248bccfe60de83f67dc59a21e6d62cc472db68ca
KiriIl <375447360162@mail.ru>
2023-02-28 14:10:36 +0300

    add models

---
+++ StepsStatistic/StepsStatistic/Models/StatModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StepsStatistic.Models
+{
+    public class StatModel
+    {
+        public int Rank { get; set; }
+        public string Status { get; set; }
+        public int Steps { get; set; }
+    }
+}
---
+++ StepsStatistic/StepsStatistic/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StepsStatistic.Models
+{
+    public class UserModel
+    {
+        public string User { get; set; }
+        public List<StatModel> Stats { get; set; }
+        public double AvgSteps => Stats.Average(x => x.Steps);
+        public int BestResult => Stats.Max(x => x.Steps);
+        public int WorstResult => Stats.Min(x => x.Steps);
+    }
+}
