cf431f372627188ee53061be025e393fbd20fb7a
KiriIl <375447360162@mail.ru>
2023-02-28 14:50:01 +0300

    added interface IDialogService

---
+++ StepsStatistic/StepsStatistic/Services/Abstraction/IDialogService.cs
@@ -0,0 +1,12 @@
+﻿using StepsStatistic.Utils;
+
+namespace StepsStatistic.Services.Abstraction
+{
+    public interface IDialogService
+    {
+        FilePath FileToSave { get; }
+        FilePath[] FilesToOpen { get; }
+        bool OpenFilesDialog();
+        bool SaveFileDialog();
+    }
+}
