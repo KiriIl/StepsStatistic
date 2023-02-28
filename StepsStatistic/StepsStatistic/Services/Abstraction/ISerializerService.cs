aaf4895b82d04b66d1dd18e8f21f32f61671eabe
KiriIl <375447360162@mail.ru>
2023-02-28 14:32:19 +0300

    added interface ISerializer

---
+++ StepsStatistic/StepsStatistic/Services/Abstraction/ISerializerService.cs
@@ -0,0 +1,12 @@
+﻿using StepsStatistic.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StepsStatistic.Services.Abstraction
+{
+    public interface ISerializerService<TModel>
+    {
+        Task<IEnumerable<TModel>> DeserializeAsync(FilePath filePath);
+        Task SerializeAsync(FilePath filePath, TModel model);
+    }
+}
---
+++ StepsStatistic/StepsStatistic/Utils/FilePath.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace StepsStatistic.Utils
+{
+    public class FilePath
+    {
+        public string FullPath { get; }
+        public string FileName { get => Path.GetFileNameWithoutExtension(FullPath); }
+        public string FullFileName { get => Path.GetFileName(FullPath); }
+
+        public FilePath(string fullPath)
+        {
+            FullPath = fullPath;
+        }
+    }
+}
