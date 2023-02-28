f05a0913bd7f934891094391cd07615bdb3babc3
KiriIl <375447360162@mail.ru>
2023-02-28 12:06:12 +0300

    init project

---
+++ StepsStatistic/StepsStatistic/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StepsStatistic.Views;
+using System.Windows;
+
+namespace StepsStatistic
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new StatisticWindow();
+            MainWindow.Show();
+        }
+    }
+}
---
+++ StepsStatistic/StepsStatistic/Views/StatisticWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace StepsStatistic.Views
+{
+    public partial class StatisticWindow : Window
+    {
+        public StatisticWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
