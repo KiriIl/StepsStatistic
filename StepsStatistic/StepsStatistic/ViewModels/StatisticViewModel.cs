a0cbd1825690bfadf5e7a03649ca25ecbc2a4aec
KiriIl <375447360162@mail.ru>
2023-02-28 14:12:36 +0300

    added StatisticViewModel.cs

---
+++ StepsStatistic/StepsStatistic/ViewModels/StatisticViewModel.cs
@@ -0,0 +1,25 @@
+﻿using StepsStatistic.Models;
+using System.Collections.ObjectModel;
+
+namespace StepsStatistic.ViewModels
+{
+    public class StatisticViewModel : BaseViewModel
+    {
+        private ObservableCollection<UserModel> _users;
+
+        public ObservableCollection<UserModel> Users
+        {
+            get => _users;
+            set
+            {
+                _users = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StatisticViewModel()
+        {
+            _users = new ObservableCollection<UserModel>();
+        }
+    }
+}
