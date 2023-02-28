6903e9906f3a141ee44a9ff50eeb0f968f58a4c4
KiriIl <375447360162@mail.ru>
2023-02-28 14:06:07 +0300

    added BaseViewModel class

---
+++ StepsStatistic/StepsStatistic/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StepsStatistic.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
