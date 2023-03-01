8f300c2e3661dfe9e9e312bf9f807bdeeb9c7e17
KiriIl <375447360162@mail.ru>
2023-03-01 18:39:49 +0300

    added DialogService implementation

---
+++ StepsStatistic/StepsStatistic/Services/DialogService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using StepsStatistic.Services.Abstraction;
+using StepsStatistic.Utils;
+using System.Linq;
+
+namespace StepsStatistic.Services
+{
+    public class DialogService : IDialogService
+    {
+        private FilePath _fileToSave;
+        private FilePath[] _filesToOpen;
+
+        public FilePath FileToSave => _fileToSave;
+        public FilePath[] FilesToOpen => _filesToOpen;
+
+        public bool OpenFilesDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _filesToOpen = openFileDialog.FileNames.Select(x => new FilePath(x)).ToArray();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool SaveFileDialog()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _fileToSave = new FilePath(saveFileDialog.FileName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
