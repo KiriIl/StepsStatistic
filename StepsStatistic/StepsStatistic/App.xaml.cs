69696eed1e2227a7d8b1a3ad3bef6fa6874b3ec1
KiriIl <375447360162@mail.ru>
2023-03-01 19:05:38 +0300

    implemented DI

--- StepsStatistic/StepsStatistic/App.xaml.cs
+++ StepsStatistic/StepsStatistic/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿using StepsStatistic.ViewModels;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using StepsStatistic.AutoMapper;
+using StepsStatistic.Models;
+using StepsStatistic.Services;
+using StepsStatistic.Services.Abstraction;
+using StepsStatistic.ViewModels;
 using StepsStatistic.Views;
 using System.Windows;
 
@@ -6,12 +13,33 @@ namespace StepsStatistic
 {
     public partial class App : Application
     {
+        private ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            StatisticViewModel statisticViewModel = new StatisticViewModel();
-            MainWindow = new StatisticWindow(statisticViewModel);
+            MainWindow = _serviceProvider.GetService<StatisticWindow>();
             MainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton(x => new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+            });
+            services.AddSingleton(new MapperConfiguration(serviceProvider => serviceProvider.AddProfile(new AutoMapperProfile())).CreateMapper());
+            services.AddSingleton<IDialogService, DialogService>();
+            services.AddSingleton<ISerializerService<UserModel>, UserSerializer>();
+            services.AddSingleton<StatisticViewModel>();
+            services.AddSingleton(serviceProvider => new StatisticWindow(serviceProvider.GetService<StatisticViewModel>()));
         }
     }
 }
