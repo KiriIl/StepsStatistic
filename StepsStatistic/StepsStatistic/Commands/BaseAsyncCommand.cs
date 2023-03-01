0f9a95de3dcc5dc638043a746862ee6af6fd4817
KiriIl <375447360162@mail.ru>
2023-03-01 18:43:35 +0300

    added BaseAsyncCommand.cs

---
+++ StepsStatistic/StepsStatistic/Commands/BaseAsyncCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace StepsStatistic.Commands
+{
+    public abstract class BaseAsyncCommand : BaseCommand
+    {
+        public bool IsExecuting { get; set; }
+
+        public override bool CanExecute(object parameter)
+        {
+            return !IsExecuting;
+        }
+
+        public override sealed async void Execute(object parameter)
+        {
+            IsExecuting = true;
+            await ExecuteAsync(parameter);
+            IsExecuting = false;
+        }
+
+        public abstract Task ExecuteAsync(object parameter);
+    }
+}
