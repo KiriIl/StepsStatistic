2509d525baa52cdd0e4a6dc3d4b5dae24dc463d1
KiriIl <375447360162@mail.ru>
2023-04-06 15:50:51 +0300

    added data trigger for list view items

--- StepsStatistic/StepsStatistic/Models/UserModel.cs
+++ StepsStatistic/StepsStatistic/Models/UserModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using StepsStatistic.Utils;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StepsStatistic.Models
@@ -10,5 +11,19 @@ namespace StepsStatistic.Models
         public double AvgSteps => Stats.Average(x => x.Steps);
         public int BestResult => Stats.Max(x => x.Steps);
         public int WorstResult => Stats.Min(x => x.Steps);
+        public bool IsSpecial
+        {
+            get
+            {
+                double percentValue = AvgSteps * Constants.PercentageStepDifference;
+                if (BestResult > AvgSteps + percentValue
+                    || WorstResult < AvgSteps - percentValue)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
     }
 }
---
+++ StepsStatistic/StepsStatistic/Utils/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace StepsStatistic.Utils
+{
+    public static class Constants
+    {
+        public const double PercentageStepDifference = 0.2;
+    }
+}
